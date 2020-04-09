c287894c9d697f34b28a1639f46b7e681317d0c3
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2020-04-09 16:50:06 +0600

    Updates

---
+++ precache-manifest.6becd4176dca76ca5f8708fa893195c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.714397e2.js"
+  },
+  {
+    "revision": "2892daa64ae45fd14630",
+    "url": "/todo-list/static/js/main.b67dee2d.chunk.js"
+  },
+  {
+    "revision": "b6f2b1fcb96299937f00",
+    "url": "/todo-list/static/js/2.c4532e55.chunk.js"
+  },
+  {
+    "revision": "2892daa64ae45fd14630",
+    "url": "/todo-list/static/css/main.2f41dea7.chunk.css"
+  },
+  {
+    "revision": "b6f2b1fcb96299937f00",
+    "url": "/todo-list/static/css/2.4200bd1e.chunk.css"
+  },
+  {
+    "revision": "2f0aef15436b84901917cca14cb441d4",
+    "url": "/todo-list/index.html"
+  }
+];
